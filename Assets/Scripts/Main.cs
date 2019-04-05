4e6103353c0e23e881b707e10c982731a4ca6b08
unknown <33580@ETECAF.LOCAL>
2019-04-05 17:43:37 -0300

    array

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	void Start () {
+        
+        int[] c = Calc(3, 2);
+        Debug.Log("Soma = " + c[0]);
+        Debug.Log("Subtração = " + c[1]);
+        Debug.Log("Multiplicação = " + c[2]);
+        Debug.Log("Divisão = " + c[3]);
+	}
+
+	int[] Calc(int x, int y) {
+        int[] c = new int[4]; 
+        c[0] = x + y;
+        c[1] = x - y;
+        c[2] = x * y;
+        c[3] = x / y;
+        return c;
+	}
+}
---
+++ Assets/Scripts/Main2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main2 : MonoBehaviour {
+
+	void Start () {
+        int[,] m = Matriz(1);
+        int[][] n = ArrayArray(1);
+        Debug.Log(n[0][0]);
+        Debug.Log(m[0, 0]);
+	}
+	
+	int[][] ArrayArray (int n) {
+
+        int[][] arr = new int[3][];
+
+        arr[0] = new int[] { n };
+        arr[1] = new int[] { n + 1, n + 2 };
+        arr[2] = new int[] { n + 3, n + 4, n + 5 };
+        return arr;
+	}
+
+    int[,] Matriz(int m) {
+        int[,] arr = new int[,] {
+            { m, 0, 0 },
+            { m+1, m+2, 0 },
+            { m+3, m+4, m+5 },
+        };
+        return arr;
+    }
+}
